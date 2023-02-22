3a63fb058460b70fc2e968275cbf4ea18005e028
JUNU <bokjunwoo@gmail.com>
2023-02-23 01:55:03 +0900

    feat : react router 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,16 @@
+import SignUp from '@/pages/SignUp';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
