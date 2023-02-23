74f89eddf59d1fda1e4092f4aef970591ff1c7c8
JUNU <bokjunwoo@gmail.com>
2023-02-24 03:30:45 +0900

    feat : 수정, 삭제, 체크박스 기능 구현

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,19 +1,107 @@
-import { ITodo } from '@/pages/TodoPage/types';
-import React, { useState } from 'react';
+import { deleteTodo, updateCheck, updateTodo } from '@/api/todo';
+import { ITodoItem } from '@/pages/TodoPage/types';
+import { useCallback, useState } from 'react';
 
-const TodoItem = ({ todo }: { todo: ITodo }) => {
-  const [isComplete, setIsComplete] = useState(todo.isCompleted);
+const TodoItem = ({ todo, getTodos }: ITodoItem) => {
+  const [isClick, setIsClick] = useState(false);
+
+  // 글 수정
+  const [text, setText] = useState(todo.todo);
+  const onChangeText = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  }, []);
+
+  // 수정 버튼
+  const [amendButton, setAmendbutton] = useState(true);
+  const amendButtonChange = useCallback(() => {
+    setAmendbutton((prev) => !prev);
+  }, []);
+
+  // 글 수정 폼
+  const onSubmitForm = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!isClick) {
+        setIsClick(true);
+        updateTodo(text, todo.id, todo.isCompleted)
+          .then(() => {
+            getTodos();
+            setAmendbutton(true);
+            setText(text);
+            setIsClick(false);
+          })
+          .catch((err) => {
+            alert(err.response.data.log || err.log);
+            setIsClick(false);
+          });
+      }
+    },
+    [isClick, text, todo.id, todo.isCompleted, getTodos],
+  );
+
+  const removeTodo = useCallback(() => {
+    if (!isClick) {
+      setIsClick(true);
+      deleteTodo(todo.id)
+        .then(() => {
+          alert('삭제되었습니다.');
+          getTodos();
+          setIsClick(false);
+        })
+        .catch((err) => {
+          alert(err.response.data.log || err.log);
+          setIsClick(false);
+        });
+    }
+  }, [isClick, todo.id, getTodos]);
+
+  const checkboxChange = useCallback(() => {
+    updateCheck(text, todo.id, todo.isCompleted)
+      .then(() => {
+        getTodos();
+      })
+      .catch((err) => {
+        alert(err.response.data.log || err.log);
+      });
+  }, [getTodos, text, todo.id, todo.isCompleted]);
 
   return (
     <li>
       <label>
         <input
           type="checkbox"
-          checked={isComplete}
-          onChange={() => setIsComplete((curr) => !curr)}
+          checked={todo.isCompleted}
+          onChange={checkboxChange}
         />
-        <span>{todo.todo}</span>
       </label>
+
+      {amendButton ? (
+        <>
+          <span>{todo.todo}</span>
+          <button data-testid="modify-button" onClick={amendButtonChange}>
+            수정
+          </button>
+          <button data-testid="delete-button" onClick={removeTodo}>
+            삭제
+          </button>
+        </>
+      ) : (
+        <form onSubmit={onSubmitForm}>
+          <input
+            type="text"
+            value={text}
+            onChange={onChangeText}
+            data-testid="modify-input"
+            required
+          />
+          <button data-testid="submit-button" disabled={isClick}>
+            {isClick ? '제출중' : '제출'}
+          </button>
+          <button onClick={amendButtonChange} data-testid="cancel-button">
+            취소
+          </button>
+        </form>
+      )}
     </li>
   );
 };
